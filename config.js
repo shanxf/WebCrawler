4b1653b8957df3bb5e3553e1dd62affb1045fef7
shanxf <ishanxf@gmail.com>
2014-08-11 01:07:28 +0800

    init
    init

---
+++ config.js
@@ -0,0 +1,14 @@
+var mysql = require('mysql');
+exports.db = mysql.createConnection({
+  host: '127.0.0.1',
+  port: 3306,
+  database: 'sina_blog',
+  user: 'root',
+  password: 'root'
+});
+
+//博客配置
+exports.sinaBlog = {
+  url: 'http://blog.sina.com.cn/u/1776757314' //博客首页地址
+};
+
